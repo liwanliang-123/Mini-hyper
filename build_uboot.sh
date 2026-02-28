04a52a8e772009a3184bf5aca97046961222dee1
liwl <liwl@github.com>
2026-02-28 14:52:55 +0800

    update build_uboot.sh && delete u-boot/

--- build_uboot.sh
+++ build_uboot.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cd u-boot
+# 如果 u-boot 目录不存在，则克隆
+if [ ! -d "u-boot" ]; then
+    echo "u-boot 不存在"
+    git clone git://git.denx.de/u-boot.git
+fi
+
+cd u-boot || { echo "进入 u-boot 目录失败"; exit 1; }
+
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- qemu_arm64_defconfig
 
 # Set preboot
